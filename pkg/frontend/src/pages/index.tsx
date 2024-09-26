11752456be553f8710ddb3043de6b9e1145802c6
TheoryOfNekomata <allan.crisostomo@outlook.com>
2024-09-26 09:28:20 +0800

    feat: add frontend module
    Nextjs has been used for the frontend of letterbooked. Reasons are:
    
    - Robust enough to cater to most features
    - Familiarity with the API
    - Relatively straightforward scaffolding and setup

---
+++ pkg/frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      Letterbooked
+    </div>
+  );
+}
+
+export default IndexPage;
